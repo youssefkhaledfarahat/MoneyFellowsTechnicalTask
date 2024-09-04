c9062a647b33c6a793de357b247f98c082fe6680
Youssef Farahat <74507015+youssefkhaledfarahat@users.noreply.github.com>
2024-09-04 09:43:30 +0300

    Spacing

--- MoneyFellows.Products.Core/Interfaces/IProductRepository.cs
+++ MoneyFellows.Products.Core/Interfaces/IProductRepository.cs
@@ -5,9 +5,13 @@ namespace MoneyFellows.Products.Core.Interfaces
     public interface IProductRepository
     {
         Task<Product> GetByIdAsync(int id);
+
         Task<IEnumerable<Product>> GetAllAsync();
+
         Task AddAsync(Product product);
+
         Task UpdateAsync(Product product);
+
         Task DeleteAsync(int id);
     }
 }
